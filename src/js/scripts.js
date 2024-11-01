4c1f1c4baaf36d56284afcb8b639a161fff3e184
James Grieve <jgrieve@ualberta.ca>
2024-10-31 19:10:37 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,10 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  // Use parentheses to make sure the input is resolved before trying to modify it.
+  const name = (await input("Please enter your name: ")).trim();
+  const age = (await input("Please enter your age: ")).trim();
+  const clowns = (await input("Do you like clowns? (yes/no): ")).toLowerCase().trim();
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  output(`Hello ${name}! I see you are ${age} years old and on the question of clowns, you answered ${clowns}.`);
 }
