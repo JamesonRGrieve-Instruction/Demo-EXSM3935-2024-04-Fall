93eb1ff934a58d7a9d0dd7f2c6305fa81320270d
James Grieve <jgrieve@ualberta.ca>
2024-11-06 19:28:04 -0700

    Discrete values and switches;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,43 +2,28 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  const number = await input("Enter a number: ");
-  // output("Number entered: " + number, "debug");
-  // output(`number > 9 && number < 100: ${number > 9 && number < 100}`, "debug");
-  // if (number > 9 && number < 100) {
-  //   output("Number is two digits");
-  // } else {
-  //   if (number > 99) {
-  //     output("Number is three or more digits");
-  //   }
-  //   else
-  //   {
-  //     output("Number is one digits");
-  //   }
-  // }
-
-  // These two are exactly the same. Due to a syntactic quirk, you can omit the code block if
-  // you have only one statement in an if/else block, allowing you to chain "if" and "else".
-  if (number > 9) {
-    if (number > 99) {
-      output("Number is three or more digits");
-    }
-    else {
-      output("Number is two digits");
-    }
-  } 
-  else {
-    output("Number is one digits");
-  }
-
-  if (number > 99) {
-    output("Number is three or more digits");
-  }
-  else if (number > 9) {
-    output("Number is two digits");
-  }
-  else {
-    output("Number is one digits");
+  output("Welcome\n1. Hello\n2. Goodbye");
+  const number = await input("Enter a choice: ");
+  if (number === "1" || number === "one") {
+    output("Hello");
+  } else if (number === "2" || number === "two") {
+    output("Goodbye");
+  } else {
+    output("Invalid choice");
   }
   
+  switch (number) {
+    case "one":
+    case "1":
+      output("Hello");
+      break;
+    case "two":
+    case "2":
+      output("Goodbye");
+      break;
+    default:
+      output("Invalid choice");
+      break;
+  }
+
 }
