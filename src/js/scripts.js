707873cabde54849da9ea37bf2322f1299b38119
James Grieve <jgrieve@ualberta.ca>
2024-11-06 19:23:04 -0700

    Simplifying logic by starting with the most specific case;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,27 +3,39 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   const number = await input("Enter a number: ");
-  output("Number entered: " + number, "debug");
-  output(`number > 9 && number < 100: ${number > 9 && number < 100}`, "debug");
-  if (number > 9 && number < 100) {
-    output("Number is two digits");
-  } else {
-    if (number > 99) {
-      output("Number is three or more digits");
-    }
-    else
-    {
-      output("Number is one digits");
-    }
-  }
+  // output("Number entered: " + number, "debug");
+  // output(`number > 9 && number < 100: ${number > 9 && number < 100}`, "debug");
+  // if (number > 9 && number < 100) {
+  //   output("Number is two digits");
+  // } else {
+  //   if (number > 99) {
+  //     output("Number is three or more digits");
+  //   }
+  //   else
+  //   {
+  //     output("Number is one digits");
+  //   }
+  // }
 
   // These two are exactly the same. Due to a syntactic quirk, you can omit the code block if
   // you have only one statement in an if/else block, allowing you to chain "if" and "else".
-  if (number > 9 && number < 100) {
-    output("Number is two digits");
+  if (number > 9) {
+    if (number > 99) {
+      output("Number is three or more digits");
+    }
+    else {
+      output("Number is two digits");
+    }
   } 
-  else if (number > 99) {
+  else {
+    output("Number is one digits");
+  }
+
+  if (number > 99) {
     output("Number is three or more digits");
+  }
+  else if (number > 9) {
+    output("Number is two digits");
   }
   else {
     output("Number is one digits");
