913bdab44378e39102253467e81e1c89fae0cd16
James Grieve <jgrieve@ualberta.ca>
2024-11-07 19:07:57 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,13 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  const firstName = await input("What is your first name?: ");
+  const lastName = await input("What is your last name?: ");
+  let fullName = `${firstName} ${lastName}`; // firstName + " " + lastName;
+  fullName = fullName.replaceAll("o", "a");
+  output(fullName);
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  const largeNumber = await input("Please enter a larger number than 10: ");
+  const smallNumber = await input("Please enter a smaller number than 10: ");
+  output(`The remainder of ${largeNumber} / ${smallNumber} is ${largeNumber % smallNumber}.`);
 }
