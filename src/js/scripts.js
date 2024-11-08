7707db6cad7aa9dd0758d6ad05fcfb73046fedf2
James Grieve <jgrieve@ualberta.ca>
2024-11-07 19:10:55 -0700

    Condensing solution into two lines;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,7 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  const firstName = await input("What is your first name?: ");
-  const lastName = await input("What is your last name?: ");
-  let fullName = `${firstName} ${lastName}`; // firstName + " " + lastName;
-  fullName = fullName.replaceAll("o", "a");
-  output(fullName);
-
-  const largeNumber = await input("Please enter a larger number than 10: ");
-  const smallNumber = await input("Please enter a smaller number than 10: ");
-  output(`The remainder of ${largeNumber} / ${smallNumber} is ${largeNumber % smallNumber}.`);
+  output(`${await input("What is your first name?: ")} ${await input("What is your last name?: ")}`.replaceAll("o", "a"));
+  
+  output(`The remainder is ${await input("Please enter a larger number than 10: ") % await input("Please enter a smaller number than 10: ")}.`);
 }
