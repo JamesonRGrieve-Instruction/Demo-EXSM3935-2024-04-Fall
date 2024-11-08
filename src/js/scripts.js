a1063e19136a6fe352cea01de3553112d434ea91
James Grieve <jgrieve@ualberta.ca>
2024-11-07 19:17:13 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,27 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  const username = (await input("Enter your username: ")).trim();
+  if (!username) { //username.trim.length === 0
+    output("Username cannot be empty.");
+  }
+  else if (username !== "admin") {
+    output("Invalid username, user does not exist.");
+  }
+  else
+  {
+    const password = (await input("Enter your password: ")).trim();
+    if (!password) {
+      output("Password cannot be empty.");
+    }
+    else if (password.length < 8) {
+      output("Password must be at least 8 characters long.");
+    }
+    else if (password !== "adminpwd") {
+      output("Invalid password.");
+    }
+    else {
+      output("Login successful.");
+    }
   }
 }
