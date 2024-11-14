a2b2f8b477bf9bd91c41939a1420c8987ba075d0
James Grieve <jgrieve@ualberta.ca>
2024-11-13 19:23:19 -0700

    Throwing and catching exceptions;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,24 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  try {
+    const userAge = (await input("What is your age?: ")).trim();
+    if (userAge.length === 0) {
+      throw new Error("You did not enter an age.");
+    } 
+    else if (isNaN(Number(userAge))) {
+      throw new Error("You did not enter a number.");
+    }  
+    else if (userAge < 18) {
+      throw new Error("You are not old enough to vote.");
+    } 
+    else {
+      output("You are old enough to vote.");
+    }
   }
+  catch (error) {
+    output(error);
+  }
+  output("Program complete.")
+  
 }
