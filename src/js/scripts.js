e446eb5ce93bc2c2e32560eea84181008049c9f8
James Grieve <jgrieve@ualberta.ca>
2024-11-20 19:22:28 -0700

    Copying an array with spread syntax;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,20 +3,10 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   const myArray = [1, 2, 3];
+
+  const copyOfMyArray = [...myArray];
+
+  output(copyOfMyArray);
   myArray.push(4);
-  myArray.unshift(0);
-  output(`My Array: ${myArray}`);
-
-  output(`Length of Array: ${myArray.length}`);
-
-  output(`First Element: ${myArray[0]}`);
-  output(`Last Element: ${myArray[myArray.length - 1]}`);
-  myArray.shift();
-  output(`Last Element (Static): ${myArray[4]}`);
-
-  output(`My Array Before Splice: ${myArray}`);
-  output(`Index of 2: ${myArray.indexOf(2)}`);
-  myArray.splice(myArray.indexOf(2), 1);
-  output(`My Array After Splice: ${myArray}`);
-
+  output(copyOfMyArray);
 }
