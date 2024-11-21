b003ec397e952096edc4bf99d9c2b4ae604c61b1
James Grieve <jgrieve@ualberta.ca>
2024-11-20 19:09:35 -0700

    Reference vs data assignment;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,27 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  let myFirstNumber = 42;
+  let mySecondNumber = myFirstNumber;
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  output(`My First Number: ${myFirstNumber}`);
+  output(`My Second Number: ${mySecondNumber}`);
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  myFirstNumber = myFirstNumber + 10;
+
+  output(`My First Number After Addition: ${myFirstNumber}`);
+  output(`My Second Number After Addition: ${mySecondNumber}`);
+
+
+  const myFirstArray = [1, 2, 3];
+  const mySecondArray = myFirstArray;
+
+  output(`My First Array: ${myFirstArray}`);
+  output(`My Second Array: ${mySecondArray}`);
+
+  myFirstArray.push(4);
+
+  output(`My First Array After Push: ${myFirstArray}`);
+  output(`My Second Array After Push: ${mySecondArray}`);
+
 }
