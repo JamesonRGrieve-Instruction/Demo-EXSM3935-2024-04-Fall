35d9491d8019216588cc1dce48b961e80650b827
James Grieve <jgrieve@ualberta.ca>
2024-11-20 19:43:36 -0700

    Dictionaries intro;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,19 +2,20 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  const myArray = ["Hello", "alpha", "Bravo"];
-  myArray.push("alpha");
-  const mySet = new Set(myArray);
-  output(Array.from(mySet));
+  const myDictionary = {
+    "a": "Hello", 
+    "b": "alpha", 
+    "c": "Bravo"
+  };
 
-  mySet.add("Bravo");
+  output(myDictionary["a"]);
 
-  const myEmptySet = new Set();
-  myEmptySet.add("alpha");
-  myEmptySet.add("Bravo");
-  output(Array.from(myEmptySet));
+  myDictionary["a"] = await input("Please enter a new value: "); // Overwrite a key (works with arrays too).
+  output(myDictionary["a"]);
+  output(myDictionary); // Gives [object Object] which is entirely useless to everyone.
+  output(JSON.stringify(myDictionary)); // Converts the dictionary to JavaScript Object Notation.
 
-  output(mySet.has("alpha"));
-  mySet.delete("alpha");
-  output(mySet.has("alpha"));
+  const username = "d";
+  output(Boolean(myDictionary[username]));
+
 }
