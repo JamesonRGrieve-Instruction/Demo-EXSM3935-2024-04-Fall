7485d2a9e273db5d065d22d803c4160b6270744b
James Grieve <jgrieve@ualberta.ca>
2024-11-21 19:13:19 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,34 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  try {
+    const orderNumber = (await input('What is your order number?: ')).trim();
+    const orders = new Set();
+    const orderQuantites = [];
+    if (orderNumber.length != 6) {
+      throw new Error('Order number must be 6 characters long.');
+    }
+    if (!/^[a-zA-Z0-9]+$/.test(orderNumber)) {
+      throw new Error('Order number must be alphanumeric.');
+    }
+    if (!orders.has(orderNumber)) {
+      orders.add(orderNumber);
+    }
+    else {
+      throw new Error('Order number already exists.');
+    }
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+    const quantity = (await input('What is the quantity?: ')).trim();
+    if (isNaN(quantity) || !Number.isInteger(Number(quantity))) {
+      throw new Error('Quantity must be a whole number.');
+    }
+    if (quantity < 1 || quantity > 100) {
+      throw new Error('Quantity must be between 1 and 100.');
+    }
+    orderQuantites.push(quantity);
+  }
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  catch (error) {
+    output(error.message);
   }
 }
