72d4c9a0689a4a82acf7d20dccbc6e6b44e87c5e
James Grieve <jgrieve@ualberta.ca>
2024-11-21 19:05:22 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,33 +2,46 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  let totalCost = (await input("What is the total cost of the goods?: ")).trim();
-  const payment = (await input("What is the payment rendered?: ")).trim();
-  const discountCode = (await input("What is the discount code (if any)?: ")).trim();
-  switch (discountCode) {
-    case "1":
-      totalCost = totalCost * 0.9;
-      break;
-    case "2":
-      totalCost = totalCost * 0.85;
-      break;
-    case "3":
-      totalCost = totalCost * 0.75;
-      break;
-    case "4":
-      totalCost = totalCost * 0.65;
-      break;
-    case "5":
-      totalCost = totalCost * 0.6;
-      break;
-    default:
-      output("Invalid discount code, defaulting to no discount.");
+  try {
+    let totalCost = Number((await input("What is the total cost of the goods?: ")).trim());
+    if (isNaN(totalCost)) {
+      throw new Error("Invalid total cost.");
+    }
+    const payment = Number((await input("What is the payment rendered?: ")).trim());
+    if (isNaN(payment)) {
+      throw new Error("Invalid payment.");
+    }
+    const discountCode = (await input("What is the discount code (if any)?: ")).trim();
+    switch (discountCode) {
+      case "":
+        break;
+      case "1":
+        totalCost = totalCost * 0.9;
+        break;
+      case "2":
+        totalCost = totalCost * 0.85;
+        break;
+      case "3":
+        totalCost = totalCost * 0.75;
+        break;
+      case "4":
+        totalCost = totalCost * 0.65;
+        break;
+      case "5":
+        totalCost = totalCost * 0.6;
+        break;
+      default:
+        throw new Error("Invalid discount code.");
+    }
+    output("Total cost (after discount): $" + totalCost.toFixed(2));
+    if (payment < totalCost) {
+      throw new Error("Insufficient payment.");
+    }
+    else {
+      output("Change: $" + (payment - totalCost).toFixed(2));
+    }
   }
-  output("Total cost (after discount): $" + totalCost.toFixed(2));
-  if (payment < totalCost) {
-    output("Insufficient payment, remaining balance: $" + (totalCost - payment).toFixed(2));
-  }
-  else {
-    output("Change: $" + (payment - totalCost).toFixed(2));
+  catch (error) {
+    output("Error: " + error.message);
   }
 }
