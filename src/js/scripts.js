c427e7629eecf517df05a546e43c985d8d5b2544
James Grieve <jgrieve@ualberta.ca>
2024-11-27 19:47:14 -0700

    2 versions of the game board;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,27 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  const gameBoard =  [
+    ["", "", ""], // 0,0 0,1 0,2
+    ["", "", ""], // 1,0 1,1 1,2
+    ["", "", ""], // 2,0 2,1 2,2
+  ]
+  const lines = [
+    "0,0|0,1|0,2", "1,0|1,1|1,2", "2,0|2,1|2,2",  // horizontal
+    "0,0|1,0|2,0", "0,1|1,1|2,1", "0,2|1,2|2,2",  // vertical
+    "0,0|1,1|2,2", "0,2|1,1|2,0"                   // diagonal
+  ];
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  const singleArrayGameBoard = [
+    "", "", "",  // 0, 1, 2
+    "", "", "",  // 3, 4, 5
+    "", "", ""   // 6, 7, 8
+  ]
+  const singleArrayLines = [
+    "012", "345", "678",  // horizontal
+    "036", "147", "258",  // vertical
+    "048", "246"           // diagonal
+  ]
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
-}
+
+} 
