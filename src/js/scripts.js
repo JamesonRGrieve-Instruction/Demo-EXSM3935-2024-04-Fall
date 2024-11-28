aa8225acc6556eb8b039c776295dd5691af21e20
James Grieve <jgrieve@ualberta.ca>
2024-11-27 19:09:53 -0700

    Pre-test vs post-test (try 'Exit' on the first run);

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,29 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
   const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  let userInput;
+  do {
+    output("1. While Loop\n2. Do-While Loop\n3. For Loop\n4. For-Of Loop\n5. Exit");
+    userInput = (await input("Please make a selection: ")).trim();
+    if (userInput === "1") {
+      let name = await input(prompt);
+      while (name !== "Exit") {
+        output(`Hello, ${name}!`);
+        name = await input(prompt);
+      }
+    } else if (userInput === "2") {
+      let name = await input(prompt);
+      do {
+        output(`Hello, ${name}!`);
+        name = await input(prompt);
+      } while (name !== "Exit");
+    } else if (userInput === "3") {
+     
+    } else if (userInput === "4") {
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+    }
+  } while (userInput !== "5");
+
+
 }
