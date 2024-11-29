638ea6461da3b7652d58bc4fbec2cf723d957996
James Grieve <jgrieve@ualberta.ca>
2024-11-28 19:08:14 -0700

    Menu;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,19 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  let menuChoice;
+  do 
+  {
+    output("1. String Checker\n2. Integer Checker\n3. Year Checker\n4. Date Checker\n5. Exit");
+    menuChoice = (await input("Enter your choice: ")).trim();
+    if (menuChoice === "1") {
+      output("String Checker");
+    } else if (menuChoice === "2") {
+      output("Integer Checker");
+    } else if (menuChoice === "3") {
+      output("Year Checker");
+    } else if (menuChoice === "4") {
+      output("Date Checker");
+    }
+  } while (menuChoice !== "5");
 }
