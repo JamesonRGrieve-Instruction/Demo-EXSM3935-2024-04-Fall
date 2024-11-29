c907184ced0902016d45a6ee6554e4698946ee95
James Grieve <jgrieve@ualberta.ca>
2024-11-28 19:28:55 -0700

    Base requirements;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,35 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  const inventory = [
+    [1234, "Apple", 0.75],
+    [1235, "Orange", 0.99]
+  ];
+  const receipt = [];
+  let total = 0;
+  let chosenItem = "";
+  do {
+    for (let item of inventory) {
+      output(`#${item[0]} - ${item[1]} - $${item[2]}`);
+    }
+    chosenItem = (await input("What would you like to buy? (enter item code): ")).trim().toUpperCase();
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+    if (chosenItem !== "END") {
+      let found = false;
+      for (let item of inventory) {
+        if (item[0] === Number(chosenItem)) {
+          receipt.push(item[1]);
+          total += item[2];
+          found = true;
+        }
+      }
+      if (!found) {
+        output("Item not found. Please try again.");
+      }
+    }
+  } while (chosenItem !== "END");
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  output(`Receipt: ${receipt.join(", ")}`);
+  output(`Total: $${total.toFixed(2)}`);
 }
+
