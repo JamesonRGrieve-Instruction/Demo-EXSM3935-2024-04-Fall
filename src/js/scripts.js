d7b16151b4f05a33840920866617e237bfb0a94c
James Grieve <jgrieve@ualberta.ca>
2024-11-28 19:36:27 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,33 +2,74 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  try {
-    const username = (await input("Enter your username: ")).trim();
-    if (!username) { //username.trim.length === 0
-      throw new Error("Username cannot be empty.");
-    }
-    else if (username !== "admin") {
-      throw new Error("Invalid username, user does not exist.");
-    }
-    else
-    {
-      const password = (await input("Enter your password: ")).trim();
-      if (!password) {
-        throw new Error("Password cannot be empty.");
+  const users = {
+    admin: "adminpwd",
+    user: "userpwd1"
+  }
+  output("a. Register\nb. Login\nc. Quit");
+  let choice = (await input("Enter your choice: ")).trim().toLowerCase();
+  if (choice === "a") {
+    try {
+      const username = (await input("Enter your username: ")).trim();
+      if (!username) { //username.trim.length === 0
+        throw new Error("Username cannot be empty.");
       }
-      else if (password.length < 8) {
-        throw new Error("Password must be at least 8 characters long.");
-      }
-      else if (password !== "adminpwd") {
-        throw new Error("Invalid password.");
+      else if (users[username]) {
+        throw new Error("Invalid username, user already exists.");
       }
       else {
-        output("Login successful.");
+        const password = (await input("Enter your password: ")).trim();
+        if (!password) {
+          throw new Error("Password cannot be empty.");
+        }
+        else if (password.length < 8) {
+          throw new Error("Password must be at least 8 characters long.");
+        }
+        else {
+          users[username] = password;
+          output("Registration successful.");
+        }
       }
     }
+    catch (error) {
+      output(error.message);
+    }
   }
-  catch (error) {
-    output(error.message);
+  else if (choice === "b") {
+    try {
+      const username = (await input("Enter your username: ")).trim();
+      if (!username) { //username.trim.length === 0
+        throw new Error("Username cannot be empty.");
+      }
+      else if (!users[username]) {
+        throw new Error("Invalid username, user does not exist.");
+      }
+      else {
+        const password = (await input("Enter your password: ")).trim();
+        if (!password) {
+          throw new Error("Password cannot be empty.");
+        }
+        else if (password.length < 8) {
+          throw new Error("Password must be at least 8 characters long.");
+        }
+        else if (users[username] !== password) {
+          throw new Error("Invalid password.");
+        }
+        else {
+          output("Login successful.");
+        }
+      }
+    }
+    catch (error) {
+      output(error.message);
+    }
   }
+  else if (choice === "c") {
+    output("Goodbye.");
+  }
+  else {
+    output("Invalid choice.");
+  }
+
 
 }
