0da468f668195077efd07765851e93a254c8db6a
James Grieve <jgrieve@ualberta.ca>
2024-12-04 19:26:54 -0700

    Multi-parameter functions;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,21 +2,21 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
   const yearOfBirth = await input('What year were you born?: ');
-  const age = calculateAgeFromBirthYear /* Function name */(yearOfBirth /* Argument */); // Calling (or invoking) a function
+  const monthOfBirth = await input('What month were you born (number)?: ');
+  const dayOfBirth = await input('What day were you born?: ');
+  const age = calculateAgeFromBirthYear(yearOfBirth, monthOfBirth, dayOfBirth); // Calling (or invoking) a function
   output(`You are ${age} years old.`);
-
 }
+
+function calculateAgeFromBirthYear(birthYear, birthMonth, birthDay) {
+  return ((new Date()) - new Date(birthYear, birthMonth, birthDay)) / 1000 / 60 / 60 / 24 / 365.25;
+}
+
+
 
 function exampleFunction(/* inputs */) { // Inputs are also optional in the event they aren't necessary for function, but not having them is very rare, as it will make the function behave identically every time.
 
 
   return /* output */ // Output / return is optional - if you don't need anything out (and sent back to where called), omit it.
 }
-
-function calculateAgeFromBirthYear(birthYear /* Parameter */) {
-  return 2024 - birthYear /* Return value */;
-}
-
