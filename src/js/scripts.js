688f97fbd3e3442307f6523b1b9424d17b53aace
James Grieve <jgrieve@ualberta.ca>
2024-12-04 19:11:41 -0700

    Verbose comments of functions;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,19 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  const yearOfBirth = await input('What year were you born?: ');
+  const age = calculateAgeFromBirthYear /* Function name */(yearOfBirth /* Argument */); // Calling (or invoking) a function
+  output(`You are ${age} years old.`);
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
 }
+
+function exampleFunction(/* inputs */) { // Inputs are also optional in the event they aren't necessary for function, but not having them is very rare, as it will make the function behave identically every time.
+
+
+  return /* output */ // Output / return is optional - if you don't need anything out (and sent back to where called), omit it.
+}
+
+function calculateAgeFromBirthYear(birthYear /* Parameter */) {
+  return 2024 - birthYear /* Return value */;
+}
+
