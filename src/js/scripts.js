9e4c7898da388641d261070c7ea804e35a5594c0
James Grieve <jgrieve@ualberta.ca>
2024-12-05 19:20:46 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,33 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  greeter(await getName(), await piggyBank());
+}
+async function getName() {
+  const name = (await input("What is your name?: ")).trim();
+  return name || "child1"; // If the left side is falsey, return the right side.
+}
+async function piggyBank() {
+  try {
+    const balance = (await input("Enter a list of comma-separated deposits: ")).trim();
+    let total = 0;
+    for (const deposit of balance.split(",")) {
+      total += Number(deposit);
+    }
+    return total;
+  } catch (error) {
+    output("Invalid input, assuming no deposits.");
+    return 0;
   }
 }
+function greeter(name, amount) {
+  if (amount < 10) {
+    output(`Hello ${name}! You’ve stored $${amount.toFixed(2)} so far. Better keep saving!`)
+  }
+  else if (amount < 100) {
+    output(`Hello ${name}! You’ve stored $${amount.toFixed(2)} so far. Good job!`)
+  }
+  else {
+    output(`Hello ${name}! You’ve stored $${amount.toFixed(2)} so far. You should really spend some of it!`)
+  }
+} 
